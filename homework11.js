868274b572ae6cfd77302ab03e4bb3ca54bd419c
AllayarovQuldosh <94292201+AllayarovQuldosh@users.noreply.github.com>
2021-11-20 13:44:13 -0500

    Add files via upload

---
+++ homework11.js
@@ -0,0 +1,86 @@
+//>>>>>>>>>>>>>>>>>>>>   1- example  <<<<<<<<<<<<<<<<<<<<
+
+// function fac(num){
+//     if(num === 1) {
+//         return num;
+//     // return num * fac(num - 1);
+//     }
+//     return num * fac(num - 1);
+// }
+
+// fac(5);
+// console.log(fac(4));
+
+
+//>>>>>>>>>>>>>>>>>>>>>>> 2- example <<<<<<<<<<<<<<<<<<<<<<<
+ 
+// function natija(x,y){
+//     for(let i = x+1;i < y;i++){
+//         console.log(i);
+//     }
+// }
+// natija(2,9);
+
+//>>>>>>>>>>>>>>>>>>>>>> 3- example <<<<<<<<<<<<<<<<<<<<<<<<<
+
+// let massiv = [1,2,3,4,5,6];
+// jm = 0;
+// for(let i=0;i<massiv.length;i++){
+//     for(let j=0;j<massiv;j++){
+//         j++;
+//         console.log(i[j]);
+//     }
+// }
+
+//>>>>>>>>>>>>>>>>>>>> 4- example <<<<<<<<<<<<<<<<<<<<<<<<<<
+// function nt(n){
+//     n = n**2;
+//     console.log(n);
+// }
+// nt(8);
+
+// >>>>>>>>>>>>>>>>>> 5- example <<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// function fibi(n) {
+//     return n < 1 ? 0
+//          : n <= 2 ? 1
+//          : fibi(n - 1) + fibi(n - 2)
+//  }
+ 
+//  console.log(fibi(10));// 1+1+2+3+5+8+13+21+34+55
+
+// >>>>>>>>>>>>>>>>>> 6- example <<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// function jufttoq(a){
+//     if(a % 2 == 0){
+//         console.log('Juft Son',a);
+//     }else{
+//         console.log('Toq Son',a);
+//     }
+// }
+// jufttoq(22);
+
+// >>>>>>>>>>>>>>>>> Function <<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// >>>>>>>>>>>>>>>>> 1- example<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+
+
+
+// >>>>>>>>>>>>>>>>>> 2- example <<<<<<<<<<<<<<<<<<<<<<<<<<<
+ 
+
+const str = 'weqrwiirabs';
+function sorting(str){
+    for(let i=0;i<str.length;i++){
+        sort(i);
+        console.log(i);
+    }
+}
+sorting(str)
+
+
+ 
+
+
+
